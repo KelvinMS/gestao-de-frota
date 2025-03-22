9ee70d992a61272bf6c3a61aede2ca194236d83e
kms <kelvin651b@gmail.com>
2025-03-22 15:13:04 -0300

    refactor main to use objects and separate DB methods

--- drivers_data.py
+++ drivers_data.py
@@ -1,33 +1,81 @@
 import sqlite3
 
-class DriverDataDB():
 
-    #Conntect to db
-    def connect_driver_db(self):
-        self.conn = sqlite3.connect('driver.bd')
+
+class DriverDataFuntionsDB():
+
+        #Conntect to db
+    def connect_drive_db(self):
+        self.conn = sqlite3.connect('drivers.bd')
         self.cursor = self.conn.cursor()
 
     #Disconntect to db
-    def desconnec_drive_db(self):
+    def desconnect_driver_db(self):
         self.conn.close()
 
-
-    #Create vehicle tables
-    def create_driver_db(self):
-        self.connect_vehicle_db()
+    #Create driver tables
+    def createTableDrivers(self):
+        self.connect_drive_db()
         print('Connected to DB')
         self.cursor.execute("""                                   
-            CREATE TABLE IF NOT EXISTS drive(
+            CREATE TABLE IF NOT EXISTS drivers(
                 cod INTEGER PRIMARY KEY,
                 nome CHAR(15) NOT NULL,
                 cpf CHAR(15) NOT NULL,
-                cnh_number CHAR(20) NOT NULL,
+                cnh CHAR(20) NOT NULL,
                 email CHAR(15) NOT NULL,
                 telefone CHAR(40) NOT NULL,
-                cnh_file CHAR(40) NOT NULL
+                cnh_path CHAR(40) NOT NULL
                 );
             """)
         self.conn.commit()
-        
-        print('Banco driver Criado')
+        print('Banco drivers Criado')
 
+    #Add driver to db and update treeview
+    def addDriveToDB(self,nome,cpf,cnh,email,telefone,cnh_path):       
+        self.connect_drive_db()
+        self.cursor.execute(""" INSERT INTO  drives (nome,cpf,cnh,email,telefone,cnh_path)
+                            VALUES (?,?,?,?,?,?)""", (nome,cpf,cnh,email,telefone,cnh_path))
+        self.conn.commit()
+        self.desconnect_driver_db()
+        print('Motorista Adicionado')
+
+    #Delete driver from db
+    def deleteDriverToDB(self,cod):
+        self.connect_drive_db()
+        self.cursor.execute(""" DELETE FROM drivers WHERE cod = ? """, (cod))
+        self.conn.commit()
+        self.desconnect_driver_db()
+        print('Veiculo Deletado')
+
+    #Update driver from db
+    def updateDriversDB(self,nome,cpf,cnh,email,telefone,cnh_path, cod):
+        self.connect_drive_db()
+        self.cursor.execute(""" UPDATE drivers 
+            SET  nome = ?,cpf = ?,cnh = ?,email = ?,telefone = ?,cnh_path = ? WHERE cod = ? """,
+            (nome,cpf,cnh,email,telefone,cnh_path, cod))
+        self.conn.commit()
+        self.desconnect_driver_db()
+        print('Motorista Atualizado')
+
+    def getDriversDB(self):
+        self.connect_drive_db()
+        cursorList = self.cursor.execute(""" SELECT cod,nome,cpf,cnh,email,telefone,cnh_path
+        FROM drivers ORDER BY cod ASC;""").fetchall()#fetchall() retorna uma lista com todos os registros
+        self.desconnect_driver_db()
+        if cursorList == []:
+            print('Motorista não encontrado')
+        else:
+            print('Motorista Encontratos')
+        return cursorList
+        
+    def searchDriverDB(self,nome):
+        self.connect_drive_db()
+        cursorList =self.cursor.execute(""" SELECT cod,nome,cpf,cnh,email,telefone,cnh_path
+        FROM drivers WHERE nome LIKE '%s'ORDER BY cod ASC;""" % nome).fetchall()
+        self.desconnect_driver_db()
+        if cursorList == []:
+            print('Motorista não encontrado')
+        else:
+            print('Motorista Encontrado')
+        return cursorList
--- vehicle_data.py
+++ vehicle_data.py
@@ -2,7 +2,7 @@ import sqlite3
 
 
 
-class VehicleDataDB():
+class VehicleDataFuntionsDB():
 
         #Conntect to db
     def connect_vehicle_db(self):
@@ -14,7 +14,7 @@ class VehicleDataDB():
         self.conn.close()
 
     #Create vehicle tables
-    def createTables(self):
+    def createTableVehicles(self):
         self.connect_vehicle_db()
         print('Connected to DB')
         self.cursor.execute("""                                   
@@ -32,12 +32,50 @@ class VehicleDataDB():
         print('Banco vehicles Criado')
 
     #Add vehicle to db and update treeview
-    def addVehiclesDB(self):       
+    def addVehiclesDB(self,marca,modelo,placa,quilometragem,ano,combustivel):       
         self.connect_vehicle_db()
         self.cursor.execute(""" INSERT INTO  vehicles (marca,modelo,placa,quilometragem,ano,combustivel)
-                            VALUES (?,?,?,?,?,?)""", (self.marca,self.modelo,self.placa,self.quilometragem,self.ano,self.combustivel))
+                            VALUES (?,?,?,?,?,?)""", (marca,modelo,placa,quilometragem,ano,combustivel))
         self.conn.commit()
         self.desconnect_vehicle_db()
         print('Veiculo Adicionado')
 
-    
+    #Delete vehicle from db
+    def deleteVehicleDB(self,cod):
+        self.connect_vehicle_db()
+        self.cursor.execute(""" DELETE FROM vehicles WHERE cod = ? """, (cod))
+        self.conn.commit()
+        self.desconnect_vehicle_db()
+        print('Veiculo Deletado')
+
+    #Update vehicle from db
+    def updateVehicleDB(self,marca,modelo,placa,quilometragem,ano,combustivel, cod):
+        self.connect_vehicle_db()
+        self.cursor.execute(""" UPDATE vehicles 
+            SET  marca = ?,modelo = ?,placa = ?,quilometragem = ?,ano = ?,combustivel = ? WHERE cod = ? """,
+            (marca,modelo,placa,quilometragem,ano,combustivel, cod))
+        self.conn.commit()
+        self.desconnect_vehicle_db()
+        print('Veiculo Atualizado')
+
+    def getVehiclesDB(self):
+        self.connect_vehicle_db()
+        cursorList = self.cursor.execute(""" SELECT cod,marca,modelo,placa,quilometragem,ano,combustivel
+        FROM vehicles ORDER BY cod ASC;""").fetchall()#fetchall() retorna uma lista com todos os registros
+        self.desconnect_vehicle_db()
+        if cursorList == []:
+            print('Veiculo não encontrado')
+        else:
+            print('Veiculos Encontratos')
+        return cursorList
+        
+    def searchVehicleDB(self,marca):
+        self.connect_vehicle_db()
+        cursorList =self.cursor.execute(""" SELECT cod,marca,modelo,placa,quilometragem,ano,combustivel
+        FROM vehicles WHERE marca LIKE '%s'ORDER BY cod ASC;""" % marca).fetchall()
+        self.desconnect_vehicle_db()
+        if cursorList == []:
+            print('Veiculo não encontrado')
+        else:
+            print('Veiculo Encontrado')
+        return cursorList
